6579b6262663bf0713c0e11be451a1fce77c7af8
Lukas Barth <mail@tinloaf.de>
2014-04-12 18:57:27 +0200

    Documentation

--- src/de/tinloaf/intervaltree/HasInterval.java
+++ src/de/tinloaf/intervaltree/HasInterval.java
@@ -1,5 +1,17 @@
 package de.tinloaf.intervaltree;
 
-public interface HasInterval <E extends Comparable> {
+/**
+ * This is the interface that must be implemented by anything that should be an entry in an Interval Tree.
+ * 
+ * @author Lukas Barth
+ *
+ * @param <E> Anything comparable to itself, this will make up the interval borders.
+ */
+public interface HasInterval <E extends Comparable<E>> {
+	/**
+	 * Returns the interval of the entry.
+	 * 
+	 * @return The interval of the entry.
+	 */
 	public IInterval<E> getInterval();
 }
--- src/de/tinloaf/intervaltree/IInterval.java
+++ src/de/tinloaf/intervaltree/IInterval.java
@@ -1,6 +1,24 @@
 package de.tinloaf.intervaltree;
 
-public interface IInterval <E extends Comparable> {
+/**
+ * The interval of the entries in the Interval Tree
+ * 
+ * @author Lukas Barth
+ *
+ * @param <E> Anything comparable to itself. This will make up the interval borders.
+ */
+public interface IInterval <E extends Comparable<E>> {
+	/**
+	 * Returns the begin of the interval.
+	 *
+	 * @return The begin of the interval
+	 */
 	public E getBegin();
+	
+	/**
+	 * Returns the end of the interval.
+	 * 
+	 * @return The end of the interval
+	 */
 	public E getEnd();
 }
